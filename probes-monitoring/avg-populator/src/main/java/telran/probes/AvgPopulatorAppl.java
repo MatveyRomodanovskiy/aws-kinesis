ee8b54ca051373324213b7330d2ea703fbfea8f3
MatveyRomodanovskiy <startpolus@gmail.com>
2024-04-16 01:31:50 +0300

    changes in module avg-populator (change database to aws dynamoDB)

--- probes-monitoring/avg-populator/src/main/java/telran/probes/AvgPopulatorAppl.java
+++ probes-monitoring/avg-populator/src/main/java/telran/probes/AvgPopulatorAppl.java
@@ -2,20 +2,29 @@ package telran.probes;
 
 import java.util.function.Consumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import telran.probes.dto.ProbeData;
-import telran.probes.model.ProbeDataDoc;
-import telran.probes.repo.ProbeDataRepo;
+
 @Slf4j
 @SpringBootApplication
 @RequiredArgsConstructor
 public class AvgPopulatorAppl {
-final ProbeDataRepo probeDataRepo;
+@Value("${avgTabletName}")
+String avgTableName;
+@Value("${regiontName}")
+String region;
 	public static void main(String[] args) {
 		SpringApplication.run(AvgPopulatorAppl.class, args);
 
@@ -24,10 +33,20 @@ final ProbeDataRepo probeDataRepo;
 	Consumer<ProbeData> avgPopulatorConsumer() {
 		return this::probeDataPopulation;
 	}
+	
 	void probeDataPopulation(ProbeData probeData) {
 		log.debug("received probeData: {}", probeData);
-		ProbeDataDoc probeDataDoc = probeDataRepo.save(new ProbeDataDoc(probeData));
-		log.debug("Document {} has been saved to Database", probeDataDoc);
+		AmazonDynamoDB clientAmazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+	            .withRegion(region)
+	            .build();
+		DynamoDB dynamoDB = new DynamoDB(clientAmazonDynamoDB);
+		Table table = dynamoDB.getTable(avgTableName);
+		Item item = new Item()
+				.withPrimaryKey("id", probeData.id())
+				.withDouble("value", probeData.value())
+				.withLong("timestamp", probeData.timestamp());
+		table.putItem(item);
+		log.debug("Avg value {} has been saved to Database", probeData);
 	}
 
 }
